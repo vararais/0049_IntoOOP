78fc008c42f7f919287adcf8148201e2ba5f02ae
vararais <varadigmaisnann@gmail.com>
2024-04-23 19:23:39 +0700

    Menambah Void Output

--- YangPertama/YangPertama.cpp
+++ YangPertama/YangPertama.cpp
@@ -6,6 +6,12 @@ public:
     string nama;
     int umur;
     string jurusan;
+
+    void output() {
+        cout << "Nama : " << nama << endl;
+        cout << "Umur : " << umur << endl;
+        cout << "Jurusan : " << jurusan << endl;
+    }
 };
 int main()
 {
